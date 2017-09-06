9451086c9af5caa2edb9c2a6f267a678fbdb860a
MariaP92 <100kj.mr@gmail.com>
2017-09-06 10:04:00 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,20 +16,4 @@ $(document).ready(function(){
             }
         }
     })
-   /* $('.slider-active').owlCarousel({
-        loop:true,
-        navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'], //flechas
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:1
-            }
-        }
-    })*/
 });
