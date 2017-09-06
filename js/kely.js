02cfc956aad47e5fa42f61bf967d6831cfb00a44
KelyVargaya <you@example.com>
2017-09-06 12:04:05 -0500

    Cambiando los estilos

--- js/kely.js
+++ js/kely.js
@@ -14,3 +14,20 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
 });
+
+
+$(document).ready(function(){
+    $("#send").click(function(){
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var phone = $("#phone").val();
+        var id= $("#message").val();
+        console.log(name + "e"+email + "p" +phone);
+        alert("Mensaje recibido");
+        $("#name").val("");
+        $("#email").val("");
+        $("#phone").val("");
+        $("#message").val("");
+    });
+    
+});
